feaac1cb8a7d27de8a958caff2464c99402b8699
lizengyu21 <lizengyu21@mails.tsinghua.edu.cn>
2023-07-06 02:34:36 +0800

    generate Triangle object

--- object.cu
+++ object.cu
@@ -23,3 +23,26 @@ __device__ void Sphere::intersect(const PathState &path_state, HitRecord &record
         record.normal = unit(hit_point - origin);
     }
 }
+
+__device__ void Triangle::intersect(const PathState &path_state, HitRecord &record) {
+    if (dot(path_state.ray.direction, this->normal) > 0) return;
+    float u, v, t_tmp = 0;
+    float3 p_vec = cross(path_state.ray.direction, e2);
+    float det = dot(e1, p_vec);
+    if (det < 0.00001f && det > -0.00001f) return;
+    float det_inv = 1.0f / det;
+    float3 t_vec = path_state.ray.position - v0;
+    u = dot(t_vec, p_vec) * det_inv;
+    if (u < 0 || u > 1) return;
+    float3 q_vec = cross(t_vec, e1);
+    v = dot(path_state.ray.direction, q_vec) * det_inv;
+    if (v < 0 || u + v > 1) return;
+    t_tmp = dot(e2, q_vec) * det_inv;
+    if (t_tmp > 0 && t_tmp < record.t) {
+        record.t = t_tmp;
+        record.missed = false;
+        record.normal = this->normal;
+        record.position = path_state.intersaction_point(t_tmp);
+        record.material_index = this->material_index;
+    }
+}
--- object.cuh
+++ object.cuh
@@ -3,12 +3,7 @@
 #include "record.cuh"
 #include "aabb.cuh"
 
-class Object {
-public:
-    unsigned int material_index = 0xFFFFFFFF;
-};
-
-class Sphere : Object {
+class Sphere {
 public:
     float3 origin;
     float radius;
@@ -25,4 +20,33 @@ struct sphere_aabb_getter {
     }
 };
 
+class Triangle {
+public:
+    float3 v0, v1, v2;
+    float3 e1, e2;
+    float3 normal;
+    unsigned int material_index;
+    Triangle(float3 a, float3 b, float3 c, unsigned int m_id) : v0(a), v1(b), v2(c), material_index(m_id) {
+        e1 = v1 - v0;
+        e2 = v2 - v0;
+        normal = unit(cross(e1, e2));
+    }
+    __device__ void intersect(const PathState &path_state, HitRecord &record);
+};
+
+struct triangle_aabb_getter {
+    __device__ Aabb operator() (const Triangle &t) const noexcept {
+        float x_min = min(t.v0.x, min(t.v1.x, t.v2.x));
+        float y_min = min(t.v0.y, min(t.v1.y, t.v2.y));
+        float z_min = min(t.v0.z, min(t.v1.z, t.v2.z));
+
+        float x_max = max(t.v0.x, max(t.v1.x, t.v2.x));
+        float y_max = max(t.v0.y, max(t.v1.y, t.v2.y));
+        float z_max = max(t.v0.z, max(t.v1.z, t.v2.z));
+        float3 lower = make_float3(x_min, y_min, z_min);
+        float3 upper = make_float3(x_max, y_max, z_max);
+        
+        return Aabb(lower, upper);
+    }
+};
 
